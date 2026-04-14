b84a9d9afd73500ab2eee0a0e2bba463c5d2cdca
Gaurav Saklani <gauravthe7841@gmail.com>
2026-04-14 19:45:04 +0530

    Add climbing stairs solution using dynamic programming
    Implement dynamic programming solution for climbing stairs problem.

---
+++ climibing_stairs.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/climbing-stairs/
+
+class Solution {
+public:
+    vector<int> dp = vector<int>(46, -1);
+    int climbStairs(int n) {
+        if(n <= 2) return n;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = climbStairs(n-1) + climbStairs(n-2);
+    }
+};
